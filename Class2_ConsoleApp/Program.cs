05c9e51f203b951b4547b3ea9fcaf8b6197d290c
j-hayes <jacksonjhayes@gmail.com>
2013-09-14 11:10:19 -0500

    Adding Examples From Lecture Slides 1

---
+++ Class2_ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2_ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<String> _reasons = new List<string>();
+
+
+            Console.WriteLine("Programming Can Be Fun!");
+
+
+            Console.WriteLine("Tell Me a reason Why you think Programming is fun");
+
+            _reasons.Add(Console.ReadLine());
+            Console.WriteLine("Tell Me another reason Why you think Programming is fun");
+            _reasons.Add(Console.ReadLine());
+            Console.WriteLine("Tell Me yet another reason Why you think Programming is fun");
+            _reasons.Add(Console.ReadLine());
+            Console.WriteLine("The Reasons that Programming is fun are:");
+
+            foreach (string s in _reasons)
+            {
+                Console.WriteLine(s);
+
+
+            }
+            Console.WriteLine("Please Click Enter to Exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Simple_ConsoleWrite/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_ConsoleWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming Can Be fun");
+            Console.Write("Click Enter to Exit");
+            Console.ReadLine();
+        }
+    }
+}
